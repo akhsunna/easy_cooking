c8ef3021ba52503b68475febc91ea3b1aa9f1245
Anna Molly <akhsunna4@gmail.com>
2017-03-17 22:52:40 +0200

    ingredients index

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,14 @@
 class IngredientsController < ApplicationController
   def index
+    ingredients = Dish.all
+    ingredients_count = ingredients.count
 
+    render json: {
+        dishes: ActiveModel::Serializer::CollectionSerializer.new(
+            ingredients,
+            each_serializer: IngredientSerializer
+        ),
+        count: ingredients_count
+    }
   end
 end
