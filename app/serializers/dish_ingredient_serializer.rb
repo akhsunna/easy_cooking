ea42d042538308ce01c7837a1166715d3a1861b1
Anna Molly <akhsunna4@gmail.com>
2017-03-22 23:54:44 +0200

    change id

--- app/serializers/dish_ingredient_serializer.rb
+++ app/serializers/dish_ingredient_serializer.rb
@@ -1,6 +1,10 @@
 class DishIngredientSerializer < BaseSerializer
   attributes :id, :name, :quantity, :unit
 
+  def id
+    object.ingredient.id
+  end
+
   def name
     object.ingredient.name
   end
